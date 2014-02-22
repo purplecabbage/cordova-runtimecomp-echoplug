b7bbd2e32c4bde84b72b6b14c4a9150235d63978
Jesse MacFadyen <purplecabbage@gmail.com>
2014-02-21 23:47:21 -0800

    added proxy file that calls native

---
+++ src/windows8/echopluginProxy2.js
@@ -0,0 +1,14 @@
+
+
+
+cordova.commandProxy.add("EchoPlugin",{
+    echo:function(successCallback,errorCallback,strInput) {
+        var res = EchoRuntimeComponent.EchoPluginRT.echo(strInput);
+        if(res.indexOf("Error") == 0) {
+            errorCallback(res);
+        }
+        else {
+            successCallback(res);
+        }
+    }
+});
