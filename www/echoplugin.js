96509f3d4d599eec5b7c42da5fe881455ca67517
Jesse MacFadyen <purplecabbage@gmail.com>
2014-02-24 14:24:30 -0800

    updated common-echoplugin.js to call exec which gets mapped to proxy on windows8

--- www/echoplugin.js
+++ www/echoplugin.js
@@ -1,11 +1,10 @@
 
 var EchoPlugin = {
-    echo:function(successCallback,errorCallback,strInput) {
-        if(!strInput || !strInput.length) {
-            errorCallback("Error, something was wrong with the input string. =>" + strInput);
-        }
-        else {
-            successCallback(strInput + "echo");
-        }
+    // the echo function calls successCallback with the provided text in strInput
+    // if strInput is empty, it will call the errorCallback
+    echo:function(successCallback, errorCallback, strInput) {
+        cordova.exec(successCallback,errorCallback,"EchoPlugin","echo",[strInput]);
     }
 }
+
+module.exports = EchoPlugin;
