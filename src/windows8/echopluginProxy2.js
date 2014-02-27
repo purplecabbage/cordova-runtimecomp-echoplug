00348072cc5a0a4d5928d41d1c33c76f2cd11cbb
Jesse MacFadyen <purplecabbage@gmail.com>
2014-02-26 18:17:42 -0800

    updated to install a winmd file

--- src/windows8/echopluginProxy2.js
+++ src/windows8/echopluginProxy2.js
@@ -4,6 +4,7 @@
 cordova.commandProxy.add("EchoPlugin",{
     echo:function(successCallback,errorCallback,strInput) {
         var res = EchoRuntimeComponent.EchoPluginRT.echo(strInput);
+
         if(res.indexOf("Error") == 0) {
             errorCallback(res);
         }
